bac67e4fd74949106ec80f699d730f9fe78ca1ff
Andrew Padgett <andrew.padgett.88@gmail.com>
2024-06-11 18:10:55 +0200

    installed lodash, implemented mostBlogs

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -3,6 +3,70 @@ const assert = require('node:assert')
 
 const listHelper = require('../utils/list_helper')
 
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    __v: 0,
+  },
+]
+
+const listWithSixBlogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0,
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0,
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0,
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0,
+  },
+]
+
+const listWithZeroBlogs = []
+
 test('dummy returns one', () => {
   const blogs = []
 
@@ -11,58 +75,50 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 5,
-      __v: 0,
-    },
-  ]
-
-  const listWithThreeBlogs = [
-    {
-      _id: '6666c14a8bd4d39c9b158239',
-      title: 'Blog Title',
-      author: 'Blog Author',
-      url: 'www.example.com',
-      likes: 34,
-      __v: 0,
-    },
-    {
-      _id: '666763606eceb02234783f02',
-      title: 'Blog Title 2',
-      author: 'Blog Author',
-      url: 'www.example.com',
-      likes: 343,
-      __v: 0,
-    },
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 5,
-      __v: 0,
-    },
-  ]
-
-  const listWithZeroBlogs = []
-
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     assert.strictEqual(result, 5)
   })
 
-  test('when list has three blogs, equals the sum of likes', () => {
-    const result = listHelper.totalLikes(listWithThreeBlogs)
-    assert.strictEqual(result, 382)
+  test('when list has six blogs, equals the sum of likes', () => {
+    const result = listHelper.totalLikes(listWithSixBlogs)
+    assert.strictEqual(result, 36)
   })
 
   test('when list is empty, equals zero', () => {
     const result = listHelper.totalLikes(listWithZeroBlogs)
     assert.strictEqual(result, 0)
+  })
+})
+
+describe('favorite blog', () => {
+  test('when list has only one blog, return that blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.deepStrictEqual(result, {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5,
+    })
+  })
+
+  test('when list has six blogs, return blog with highest likes', () => {
+    const result = listHelper.favoriteBlog(listWithSixBlogs)
+    assert.deepStrictEqual(result, {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12,
+    })
+  })
+
+  test('when list is empty, return empty object', () => {
+    const result = listHelper.favoriteBlog(listWithZeroBlogs)
+    assert.deepStrictEqual(result, {})
+  })
+})
+
+describe('most blogs', () => {
+  test('when list has six blogs, return author with most blogs', () => {
+    const result = listHelper.mostBlogs(listWithSixBlogs)
+    assert.strictEqual(result, 'Robert C. Martin')
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,6 @@
+const blog = require('../models/blog')
+const _ = require('lodash')
+
 const dummy = blogs => {
   return 1
 }
@@ -10,7 +13,40 @@ const totalLikes = blogs => {
       }, 0)
 }
 
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) {
+    return {}
+  } else {
+    let topBlog = blogs[0]
+    blogs.forEach(blog => {
+      if (blog.likes > topBlog.likes) {
+        topBlog = blog
+      }
+    })
+    return {
+      title: topBlog.title,
+      author: topBlog.author,
+      likes: topBlog.likes,
+    }
+  }
+}
+
+const mostBlogs = blogs => {
+  if (blogs.length === 0) {
+    return ''
+  } else {
+    const authorCounts = _.countBy(blogs, 'author')
+    const mostFrequentAuthor = _.maxBy(
+      Object.keys(authorCounts),
+      author => authorCounts[author]
+    )
+    return mostFrequentAuthor
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
 }
