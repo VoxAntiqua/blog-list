993544d9c3bde0d4b8143bbe4d4ee4be11eeabf8
Andrew Padgett <andrew.padgett.88@gmail.com>
2024-06-18 17:19:14 +0200

    implemented correct number of posts test

--- tests/blog_list_api.test.js
+++ tests/blog_list_api.test.js
@@ -1,15 +1,32 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-test('GET returns correct number of blog posts as json', async () => {
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of helper.listWithSixBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+test('GET returns blog posts as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('GET returns correct number of blog posts', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, helper.listWithSixBlogs.length)
 })
 
 after(async () => {
