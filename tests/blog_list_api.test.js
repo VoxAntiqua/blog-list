760e431a670eace2f2bf7a14a897ee3dd706f3fa
Andrew Padgett <andrew.padgett.88@gmail.com>
2024-06-19 13:14:16 +0200

    organized tests by describe

--- tests/blog_list_api.test.js
+++ tests/blog_list_api.test.js
@@ -18,89 +18,95 @@ describe('when database initially has blogs saved', () => {
     }
   })
 
-  test('GET returns blog posts as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+  describe('retrieving entries', () => {
+    test('GET returns blog posts as json', async () => {
+      await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
+
+    test('GET returns correct number of blog posts', async () => {
+      const response = await helper.blogsInDb()
+      assert.strictEqual(response.length, helper.listWithSixBlogs.length)
+    })
+
+    test('Unique identifier is named "id"', async () => {
+      const response = await helper.blogsInDb()
+      assert(Object.hasOwn(response[0], 'id'))
+    })
   })
 
-  test('GET returns correct number of blog posts', async () => {
-    const response = await helper.blogsInDb()
-    assert.strictEqual(response.length, helper.listWithSixBlogs.length)
+  describe('adding entries', () => {
+    test('New blog is correctly added', async () => {
+      const newBlog = {
+        title: 'Test Addition',
+        author: 'Homer Simpson',
+        url: 'http://example.com/',
+        likes: 0,
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const response = await helper.blogsInDb()
+
+      const titles = response.map(r => r.title)
+
+      assert.strictEqual(response.length, helper.listWithSixBlogs.length + 1)
+      assert(titles.includes('Test Addition'))
+    })
+
+    test('Added blog with no likes property defaults to 0', async () => {
+      const newBlog = {
+        title: 'Test Addition',
+        author: 'Homer Simpson',
+        url: 'http://example.com/',
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const response = await helper.blogsInDb()
+
+      assert(Object.hasOwn(response[response.length - 1], 'likes'))
+      assert.strictEqual(response[response.length - 1].likes, 0)
+    })
+
+    test('Missing title or url properties gets response of 400 Bad Request', async () => {
+      const noUrlBlog = {
+        title: 'Test Addition',
+        author: 'Homer Simpson',
+      }
+
+      const noTitleBlog = {
+        author: 'Homer Simpson',
+        url: 'http://example.com',
+      }
+
+      await api.post('/api/blogs').send(noUrlBlog).expect(400)
+      await api.post('/api/blogs').send(noTitleBlog).expect(400)
+    })
   })
 
-  test('Unique identifier is named "id"', async () => {
-    const response = await helper.blogsInDb()
-    assert(Object.hasOwn(response[0], 'id'))
-  })
+  describe('deleting entries', () => {
+    test('Can delete blog entry', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
 
-  test('New blog is correctly added', async () => {
-    const newBlog = {
-      title: 'Test Addition',
-      author: 'Homer Simpson',
-      url: 'http://example.com/',
-      likes: 0,
-    }
+      await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+      const blogsAtEnd = await helper.blogsInDb()
+      const ids = blogsAtEnd.map(b => b.id)
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    const response = await helper.blogsInDb()
-
-    const titles = response.map(r => r.title)
-
-    assert.strictEqual(response.length, helper.listWithSixBlogs.length + 1)
-    assert(titles.includes('Test Addition'))
-  })
-
-  test('Added blog with no likes property defaults to 0', async () => {
-    const newBlog = {
-      title: 'Test Addition',
-      author: 'Homer Simpson',
-      url: 'http://example.com/',
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    const response = await helper.blogsInDb()
-
-    assert(Object.hasOwn(response[response.length - 1], 'likes'))
-    assert.strictEqual(response[response.length - 1].likes, 0)
-  })
-
-  test('Missing title or url properties gets response of 400 Bad Request', async () => {
-    const noUrlBlog = {
-      title: 'Test Addition',
-      author: 'Homer Simpson',
-    }
-
-    const noTitleBlog = {
-      author: 'Homer Simpson',
-      url: 'http://example.com',
-    }
-
-    await api.post('/api/blogs').send(noUrlBlog).expect(400)
-    await api.post('/api/blogs').send(noTitleBlog).expect(400)
-  })
-
-  test('Can delete blog entry', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-    const blogToDelete = blogsAtStart[0]
-
-    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
-    const blogsAtEnd = await helper.blogsInDb()
-    const ids = blogsAtEnd.map(b => b.id)
-
-    assert(!ids.includes(blogToDelete.id))
-    assert.strictEqual(blogsAtEnd.length, blogsAtStart.length - 1)
+      assert(!ids.includes(blogToDelete.id))
+      assert.strictEqual(blogsAtEnd.length, blogsAtStart.length - 1)
+    })
   })
 })
 
