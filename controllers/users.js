78ac75cee4fddf800581af2b42c18061d77cffcb
Andrew Padgett <andrew.padgett.88@gmail.com>
2024-06-21 16:24:40 +0200

    Implemented uniqueness requirement for new usernames

--- controllers/users.js
+++ controllers/users.js
@@ -10,6 +10,15 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  if (!(password && password.length >= 3)) {
+    return response
+      .status(400)
+      .json({
+        error:
+          'User validation failed: password must be present and at least 3 characters',
+      })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
