1b0b21c82530b826aa95335ea62ee50bf2a4029d
Andrew Padgett <andrew.padgett.88@gmail.com>
2024-06-21 17:17:53 +0200

    Adding blogs designates a random user as creator, and user and blog schemas now reference one another

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,11 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs', {
+    title: 1,
+    author: 1,
+    url: 1,
+  })
   response.json(users)
 })
 
@@ -11,12 +15,10 @@ usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
   if (!(password && password.length >= 3)) {
-    return response
-      .status(400)
-      .json({
-        error:
-          'User validation failed: password must be present and at least 3 characters',
-      })
+    return response.status(400).json({
+      error:
+        'User validation failed: password must be present and at least 3 characters',
+    })
   }
 
   const saltRounds = 10
